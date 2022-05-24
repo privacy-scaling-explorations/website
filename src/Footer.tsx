d126b91408fafc493f86cea4392dd257dc3e7c06
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2022-05-24 14:46:55 -0400

    Added footer

---
+++ src/Footer.tsx
@@ -0,0 +1,5 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (<div className="footer">The Privacy and Scaling Exploration Team is a division of Ethereum Foundation © 2022</div>)
+}
