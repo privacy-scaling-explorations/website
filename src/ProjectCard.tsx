0d4b505130e49be42e3132cf351b91f92b53c343
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2022-05-24 16:29:15 -0400

    Placeholder Logos

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -3,10 +3,12 @@ import github from './images/github.svg';
 import discord from './images/discord.svg';
 import twitter from './images/twitter.svg';
 import telegram from './images/telegram.svg';
-import './ProjectCard.css'
+import './ProjectCard.css';
+import ColorHash from 'color-hash'
 
 export interface ProjectProps {
   name: string;
+  short_name?: string;
   description: string;
   image?: string;
   links?: Links[];
@@ -22,7 +24,7 @@ interface Links {
 
 function renderLink(url: string, source: string, icon: string): any {
   return (
-    <div className="link" key={url} >
+    <div className="link" key={url}>
       <a href={url} target="_blank" rel="noopener noreferrer">
         <img className="icon" src={icon} alt={source} />
         <div className="link-title">{source}</div>
@@ -31,33 +33,47 @@ function renderLink(url: string, source: string, icon: string): any {
   );
 }
 
+function renderImage(name: string): any {
+  let hueRange = { min: 45, max: 360 }
+  let colorHashText = new ColorHash({hue: hueRange, lightness: [0.8, 0.9]}).hex(name);
+  let colorHashBG = new ColorHash({hue: hueRange, lightness: [0.35, 0.4], saturation: [0.65, 0.85, 1]}).hex(name);
+  return (
+    <svg height="150px" width="350px" style={{backgroundColor: colorHashBG}} className="card-img-top">
+      <text text-anchor="middle" x="175px" y="85px" fill={colorHashText} style={{ fontSize: "2.5rem", fontStyle: "italic"}}>{name}</text>
+    </svg>
+  );
+}
+
 function ProjectCard(props: ProjectProps) {
-  let links = props.links ? props.links.map((link) => {
-    if (link.github) {
-      return renderLink(link.github, 'github', github);
-    } else if (link.website) {
-      return renderLink(link.website, 'website', globe);
-    } else if (link.discord) {
-      return renderLink(link.discord, 'discord', discord);
-    } else if (link.twitter) {
-      return renderLink(link.twitter, 'twitter', twitter);
-    } else if (link.telegram) {
-      return renderLink(link.telegram, 'telegram', telegram);
-    } else {
-      return null;
-    }
-  }) : null;
+  let links = props.links
+    ? props.links.map((link) => {
+        if (link.github) {
+          return renderLink(link.github, 'github', github);
+        } else if (link.website) {
+          return renderLink(link.website, 'website', globe);
+        } else if (link.discord) {
+          return renderLink(link.discord, 'discord', discord);
+        } else if (link.twitter) {
+          return renderLink(link.twitter, 'twitter', twitter);
+        } else if (link.telegram) {
+          return renderLink(link.telegram, 'telegram', telegram);
+        } else {
+          return null;
+        }
+      })
+    : null;
 
   let img = (() => {
     try {
       if (props.image) {
-        return (<img className="card-img-top" src={require(`./images/${props.image}`)} alt={props.name} />);
-      }
-      else {
-        return null;
+        return (
+          <img className="card-img-top" src={require(`./images/${props.image}`)} alt={props.name} />
+        );
+      } else {
+        return renderImage(props.short_name || props.name);
       }
     } catch (e) {
-      return null;
+      return renderImage(props.short_name || props.name);
     }
   })();
 
