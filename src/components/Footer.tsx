479a906a28cc0c2ae502182a09b2317aef15f5f1
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2022-05-25 09:57:51 -0400

    fixed typo in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import './Footer.css';
 
 export default function Footer() {
-  return (<div className="footer">The Privacy and Scaling Exploration Team is a division of Ethereum Foundation © 2022</div>)
+  return (<div className="footer">The Privacy and Scaling Exploration Team is a division of the Ethereum Foundation © 2022</div>)
 }
