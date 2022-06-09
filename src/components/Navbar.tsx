579d010ecca0f1b3969cfb7124ad502839876d83
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2022-06-09 17:53:37 +0000

    updated navbar with responsive menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,44 @@
+import { useState } from 'react';
 import logo from '../images/pse-logo.svg';
 import discord from '../images/discord_round.svg';
 import twitter from '../images/twitter_round.svg';
 import './Navbar.css'
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  };
+
   return (
-    <nav id="navbar">
-      <div className="nav-links">
+    <nav id="nav-menu">
+      <nav id="navbar">
+        <div id="anchor-links" className="nav-links">
+          <a href="#top">About</a>
+          <a href="#Projects">Projects</a>
+          <a href="#Team">Members</a>
+        </div>
+        <a href="#" id="pse-link">
+          <img src={logo} alt="pse logo" />
+        </a>
+        <div id="social-links" className="nav-links">
+          <a id="job-openings" href="https://jobs.lever.co/ethereumfoundation/?department=Ethereum%20Foundation&team=Privacy%20and%20Scaling%20Explorations" target="_blank" rel="noopener noreferrer">Job Openings</a>
+          <a href="https://twitter.com/PrivacyScaling" target="_blank" rel="noopener noreferrer">
+            <img src={twitter} alt="" />
+          </a>
+          <a href="https://discord.gg/g5YTV7HHbh" target="_blank" rel="noopener noreferrer">
+            <img src={discord} alt="" />
+          </a>
+        </div>
+        <div id="menu-link" onClick={() => { toggleMenu() }}>{isOpen ? 'Close' : 'Menu'}</div>
+      </nav>
+      <nav id="nav-menu-mobile" className={isOpen ? 'menu-open' : 'menu-closed'}>
         <a href="#top">About</a>
         <a href="#Projects">Projects</a>
-        <a href="#Team">Team</a>
-      </div>
-      <a href="#">
-        <img src={logo} alt="pse logo" className="pselogo" />
-      </a>
-      <div className="nav-links">
-        <a href="https://jobs.lever.co/ethereumfoundation/?department=Ethereum%20Foundation&team=Privacy%20and%20Scaling%20Explorations" target="_blank" rel="noopener noreferrer">Job Openings</a>
-        <a href="https://twitter.com/PrivacyScaling" target="_blank" rel="noopener noreferrer">
-          <img src={twitter} alt="" />
-        </a>
-        <a href="https://discord.gg/g5YTV7HHbh" target="_blank" rel="noopener noreferrer">
-          <img src={discord} alt="" />
-        </a>
-      </div>
+        <a href="#Team">Members</a>
+        <a id="job-openings" href="https://jobs.lever.co/ethereumfoundation/?department=Ethereum%20Foundation&team=Privacy%20and%20Scaling%20Explorations" target="_blank" rel="noopener noreferrer">Job Openings</a>
+      </nav>
     </nav>
   )
 }
