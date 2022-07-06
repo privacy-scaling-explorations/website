9d18591113f38dbf09f8dcdc637dfcbb85be9e75
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2022-07-06 15:48:00 +0000

    Styled modals

--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -7,7 +7,7 @@ Modal.setAppElement('#root');
 function ProjectDescription(props:any) {
   // For more descriptions modal
   const [modalIsOpen, setmodalIsOpen] = useState(false);
-  
+
   return (
     <>
       <button className="btn btn-ext" onClick={() => setmodalIsOpen(true)}>
@@ -19,7 +19,7 @@ function ProjectDescription(props:any) {
         onRequestClose={() => setmodalIsOpen(false)}
         style={{
           overlay: {
-            backgroundColor: "papayawhip",
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
           },
         }}
       >
@@ -27,12 +27,12 @@ function ProjectDescription(props:any) {
           <div>
             <h3>{props.title}</h3>
             <button className="btn btn-close" onClick={() => setmodalIsOpen(false)}>
-              Close
+
             </button>
           </div>
         </div>
         <div>
-          <p className="modal-desc">{props.long_description}</p>
+          <p className="modal-description">{props.description}</p>
         </div>
       </Modal>
     </>
