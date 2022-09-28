f8639fe81cfcf8b880f8af739324820ae0b82721
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2022-09-28 17:56:07 +0000

    Going back to browserrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,17 @@ import 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './HomePage';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './config'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route index element={<HomePage />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
